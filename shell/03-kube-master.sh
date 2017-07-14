378a719ace129cdd6d5b38bb098fe32dea32a13c
faer615 <chengfeng56@qq.com>
2017-07-14 19:28:50 +0800

    felix-zh
    修正错误1

--- shell/03-kube-master.sh
+++ shell/03-kube-master.sh
@@ -16,7 +16,7 @@ if test ! -f $kube_tar_file;then
 echo "$kube_tar_file not found!"
 else
 test ! -d $kube_pkg_dir/bin/kubernetes && cd $kube_pkg_dir/bin && tar -xvf $kube_tar_file
-cp $kube_bin_file/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl,kube-proxy,kubelet} /usr/local/bin
+cp $kube_bin_file/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl} /usr/local/bin
 rm -rf $kube_bin_file/kubernetes
 fi
 
